54bf44db2eaecb43434b8d4a815a2881dcc336d6
Chandan Dubey <dubeychandan996@gmail.com>
2022-11-23 10:56:08 +0530

    additional getlog api

---
+++ moodlegetlog.php
@@ -0,0 +1,23 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://test1.chandandubey.com/webservice/rest/server.php?wstoken=your_api&wsfunction=get_log_manager()',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: text/plain'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
